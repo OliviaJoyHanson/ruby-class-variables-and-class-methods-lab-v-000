d2f096874937e50bc49fa19ead003b773bcabd40
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-05-24 13:36:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,57 +1,30 @@
 class Song
-  attr_accessor :name, :artist, :genre
+  attr_reader :name, :artist, :genre
 
-  @@genres = []
-  @@artists = []
   @@count = 0
+  @@artists = []
+  @@genres = []
 
   def initialize(name, artist, genre)
-    @genre = genre
     @name = name
     @artist = artist
-    @@genres << @genre
-    @@artists << @artist
+    @genre = genre
     @@count += 1
+    @@artists << @artist if !@@artists.include?(@artist)
+    @@genres << @genre if !@@genres.include?(@genre)
   end
 
   def self.count
     @@count
   end
 
-  def self.genres
-    @@genres.uniq
-  end
-
   def self.artists
-    @@artists.uniq
+    @@artists
   end
 
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      count = 1
-      if genre_count[genre] == nil
-        genre_count[genre] = count
-      elsif genre_count[genre]
-        count += 1
-        genre_count[genre] = count
-      end
-    end
-    genre_count
+  def self.genres
+    @@genres
   end
 
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      count = 1
-      if artist_count[artist] == nil
-        artist_count[artist] = count
-      elsif artist_count[artist]
-        count += 1
-        artist_count[artist] = count
-      end
-    end
-    artist_count
-  end
 
 end
